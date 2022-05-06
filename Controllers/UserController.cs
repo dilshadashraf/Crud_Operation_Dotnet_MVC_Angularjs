e9c781ef3a817c16ae52772af4ce750e4abee321
Dilsad <dilshadashraf52@gmail.com>
2022-05-06 16:36:23 +0530

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using Crud_Operation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+//using Crud_Operation.Models;
+
+namespace Crud_Operation.Controllers
+{
+    public class UserController : Controller
+    {
+        //By default Get Method
+        //To View(HTML) page
+        public ActionResult AddUser()
+        {
+            return View();
+        }
+
+
+        // To submit data in to database
+        [HttpPost]
+        public ActionResult AddUser(tblUser user)// We will get the data from frontend to user object
+        {
+            CrudDBEntities db = new CrudDBEntities();// This will come from DataContext folder(Models/CRUDModel.edmx/CRUDModel.Context.tt/CRUDModel.Context.cs)
+            user.DateCreated = DateTime.Now;
+            user.CreateBy = "SuperAdmin";
+            user.IsActive = true;
+            db.tblUsers.Add(user);
+            db.SaveChanges();// To finally store in database (If we will not write it will not save data in database)
+            return View();
+        }
+
+        //To create view(HTML) page
+        public ActionResult ViewUser()
+        {
+            return View();
+        }
+
+        // To get data from database
+        public ActionResult ViewUserDetails()
+        {
+            CrudDBEntities db = new CrudDBEntities();
+            var data = db.tblUsers.ToList();
+            return Json(data, JsonRequestBehavior.AllowGet);
+            //return View();
+        }
+
+    }
+}
---
+++ Scripts/CRUDJs.js
@@ -0,0 +1,44 @@
+﻿var app = angular.module("myApp", []);
+app.controller("myCtrl", function ($scope, $http) {
+
+    //$scope.userName = "Arman";
+    //alert($scope.userName + "-" + $scope.password + "-" + $scope.email + "-" + $scope.mobile);
+    //console.log($scope.userName + "-" + $scope.password + "-" + $scope.email + "-" + $scope.mobile)
+
+
+    $scope.Register = function () {
+        $scope.clsUser = {};//Class creation
+        //C# Properties Name       UI Page ng-model name
+        $scope.clsUser.UserName = $scope.userName;
+        $scope.clsUser.Password = $scope.password;
+        $scope.clsUser.Email = $scope.email;
+        $scope.clsUser.Mobile = $scope.mobile;
+
+        //$htttp.post("/User/AddUser")
+
+        $http(
+            {
+                method: 'POST',
+                url: '/User/AddUser',
+                data: $scope.clsUser
+            }).then(function (backedresponse) {
+                alert("success");
+            })
+
+        //console.log("Class Data:" + $scope.clsUser.UserName);
+
+    }
+
+    $scope.GetUsers = function () {
+        $http({
+            method: 'GET',
+            url: '/User/ViewUserDetails'
+        }).then(function (backendResponse) {
+            console.log(backendResponse.data);
+
+            $scope.UserList = backendResponse.data;// List data to bind in html page
+        })
+    }
+
+
+});
